10ad2ebfecf264ffe0a3926de5f24fc4a59c6337
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-08-05 06:53:32 -0400

    Update emitter.h

--- KeyLimeLwCompiler/src/emitter.h
+++ KeyLimeLwCompiler/src/emitter.h
@@ -1,4 +1,13 @@
 #pragma once
 #include "parser.h"
 #include <string>
-std::string emit_nasm(const ASTNode& ast);
+#include <vector>
+#include "opcodes.h"
+#include "dg.h"
+
+struct EmitResult {
+    std::string nasm;
+    std::vector<std::string> hex_ir;
+    DGState dg_trace;
+};
+EmitResult emit_full(const ASTNode& ast);
