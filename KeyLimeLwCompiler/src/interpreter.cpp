cb1b0804c4033d463f30fed1a1dbeb46703f9bc1
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-08-05 06:55:59 -0400

    Update interpreter.cpp

--- KeyLimeLwCompiler/src/interpreter.cpp
+++ KeyLimeLwCompiler/src/interpreter.cpp
@@ -1,7 +1,28 @@
 #include "interpreter.h"
+#include "emitter.h"
+#include "hexvm.h"
+#include "dg.h"
 #include <iostream>
+#include <iomanip>
+#include "opcodes.h"
+
 void interpret(const ASTNode& ast) {
-    std::cout << "[KeyLime LW]: Running capsule...\n";
-    for (const auto& n : ast.body)
-        std::cout << "Capsule: " << (n.args.empty() ? "(unnamed)" : n.args[0]) << "\n";
+    EmitResult er = emit_full(ast);
+    std::cout << "[KeyLime LW] --- DG Trace: " << er.dg_trace.dump() << "\n";
+    std::cout << "[KeyLime LW] --- HEX IR ---\n";
+    for (const auto& line : er.hex_ir) {
+        std::string op = line.substr(0, line.find(' '));
+        bool legal = verify_opcode(op);
+        std::cout << (legal ? "[OK] " : "[!!] ") << line << "\n";
+    }
+    std::cout << "[KeyLime LW] --- Running on HexVM ---\n";
+    std::vector<HexInstr> prog;
+    for (const auto& line : er.hex_ir) {
+        size_t pos = line.find(' ');
+        std::string op = line.substr(0, pos);
+        std::string arg = (pos != std::string::npos) ? line.substr(pos+1) : "";
+        prog.push_back({op, {arg}});
+    }
+    HexVM vm;
+    vm.exec(prog);
 }
