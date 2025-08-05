d17a151da1550f4369c7f6eff5d5f60dac80784d
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-08-05 02:30:05 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,21 @@
-// src/main.cpp
-#include <iostream>
-#include "lexer.h"
-#include "parser.h"
-#include "ir_generator.h"
-#include "nasm_emitter.h"
-#include "llvm_bridge.h"
-
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cerr << "Usage: meralang <source.mera>\n";
-        return 1;
+{
+  "configurations": [
+    {
+      "inheritEnvironments": [
+        "msvc_x64"
+      ],
+      "name": "x64-Debug",
+      "includePath": [
+        "${env.INCLUDE}",
+        "${workspaceRoot}\\**"
+      ],
+      "defines": [
+        "WIN32",
+        "_DEBUG",
+        "UNICODE",
+        "_UNICODE"
+      ],
+      "intelliSenseMode": "windows-msvc-x64"
     }
-
-    std::string filePath = argv[1];
-    auto tokens = Lexer::tokenizeFile(filePath);
-    auto ast = Parser::parse(tokens);
-    auto ir = IRGenerator::generate(ast);
-
-    std::cout << "[NASM]\n";
-    std::cout << NASMEmitter::emit(ir);
-
-    std::cout << "\n[LLVM IR]\n";
-    std::cout << LLVMBridge::generateLLVM(ir);
-
-    return 0;
+  ]
 }
