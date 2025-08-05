f8492184a5278e17402690bee57874b2517163d9
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-08-05 06:52:07 -0400

    Update parser.cpp

--- KeyLimeLwCompiler/src/parser.cpp
+++ KeyLimeLwCompiler/src/parser.cpp
@@ -1,17 +1,72 @@
 #include "parser.h"
 
-ASTNode parse(const std::vector<Token>& tokens) {
-    size_t i = 0;
-    ASTNode root { "program" };
-    while (i < tokens.size()) {
-        if (tokens[i].type == TokenType::Keyword && tokens[i].lexeme == "capsule") {
-            ASTNode node{ "capsule" };
-            ++i;
-            if (tokens[i].type == TokenType::Identifier)
-                node.args.push_back(tokens[i++].lexeme);
-            root.body.push_back(node);
-        }
+static size_t i = 0;
+static const std::vector<Token>* ptoks;
+
+// Helper macros
+#define CUR (*ptoks)[i]
+#define NEXT() (++i < ptoks->size() ? (*ptoks)[i] : Token{TokenType::EndOfFile,"",0})
+
+ASTNode parse_block(); // forward
+
+ASTNode parse_val() {
+    ASTNode n{NodeKind::Val};
+    ++i; // skip 'val'
+    if (CUR.type == TokenType::Identifier) n.val = CUR.lexeme;
+    ++i;
+    if (CUR.lexeme == "=") ++i;
+    if (CUR.type == TokenType::Number) {
+        n.args.push_back(ASTNode{NodeKind::Number, CUR.lexeme});
         ++i;
+    }
+    return n;
+}
+ASTNode parse_say() {
+    ASTNode n{NodeKind::Say};
+    ++i;
+    if (CUR.type == TokenType::String)
+        n.args.push_back(ASTNode{NodeKind::String, CUR.lexeme});
+    ++i;
+    return n;
+}
+ASTNode parse_loop() {
+    ASTNode n{NodeKind::Loop};
+    ++i; // skip loop
+    if (CUR.type == TokenType::Identifier)
+        n.val = CUR.lexeme;
+    ++i; // 'from'
+    if (CUR.type == TokenType::Keyword && CUR.lexeme == "from") ++i;
+    if (CUR.type == TokenType::Number)
+        n.args.push_back(ASTNode{NodeKind::Number, CUR.lexeme});
+    ++i; // ':'
+    if (CUR.lexeme == ":") ++i;
+    n.body.push_back(parse_block());
+    return n;
+}
+ASTNode parse_block() {
+    ASTNode n{NodeKind::Block};
+    while (i < ptoks->size()) {
+        if (CUR.type == TokenType::Keyword && CUR.lexeme == "end") { ++i; break; }
+        if (CUR.type == TokenType::Keyword && CUR.lexeme == "val") n.body.push_back(parse_val());
+        else if (CUR.type == TokenType::Keyword && CUR.lexeme == "say") n.body.push_back(parse_say());
+        else if (CUR.type == TokenType::Keyword && CUR.lexeme == "loop") n.body.push_back(parse_loop());
+        else ++i;
+    }
+    return n;
+}
+ASTNode parse(const std::vector<Token>& tokens) {
+    i = 0; ptoks = &tokens;
+    ASTNode root{NodeKind::Program};
+    while (i < tokens.size()) {
+        if (CUR.type == TokenType::Keyword && CUR.lexeme == "capsule") {
+            ASTNode n{NodeKind::Capsule};
+            ++i;
+            if (CUR.type == TokenType::Identifier) { n.val = CUR.lexeme; ++i; }
+            if (CUR.lexeme == ":") ++i;
+            n.body.push_back(parse_block());
+            root.body.push_back(n);
+        }
+        else ++i;
     }
     return root;
 }
